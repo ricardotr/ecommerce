28e5abcd069beeac48a7c966f412605affefa255
ricardotr <rapkor676@gmail.com>
2025-02-03 14:32:53 +0100

    feat: creamos el dominio de Price y agregamos la configuración para la bbdd

--- src/main/java/com/demo/ecommerce/EcommerceApplication.java
+++ src/main/java/com/demo/ecommerce/EcommerceApplication.java
@@ -2,8 +2,10 @@ package com.demo.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.demo.ecommerce"})
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
